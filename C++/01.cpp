5cd2fdfddfa70e0b39e759c446c11f96bd827528
Jiramet1324 <6406021421324@fitm.kmutnb.ac.th>
2021-10-04 20:11:26 +0700

    001

---
+++ C++/01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Student Code : 088-216-1780" << endl; 
+    cout << "Student Name : Jiramet Chaihuadcharoen" << endl;
+    cout << "Division     : Information Technology" << endl;
+    cout << "Student Code : 088-216-1780" << endl;
+    cout << "Year         : 1" << endl;
+    cout << "Room         : RB" << endl;
+    cout << "Sex          : Male" << endl;
+    cout << "Age          : 21" << endl;
+    return(0);
+}
