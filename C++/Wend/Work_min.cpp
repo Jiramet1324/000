9feeaf7381f6433782dec579d5927913a6954c43
Jiramet1324 <6406021421324@fitm.kmutnb.ac.th>
2021-10-07 01:28:38 +0700

    002

--- C++/Wend/Work_min.cpp
+++ C++/Wend/Work_min.cpp
@@ -1,166 +1,296 @@
 #include <iostream>
 #include <fstream>
-using namespace std;
-void login();
-void ONA();
-void DM();
-void WM();
-void SA();
-
-//-----------------------------
-
-class LoginManager
+#include <conio.h>
+#include <string.h>
+#include<stdlib.h>
+#include<ctype.h>
+FILE *fp,*cp,*fp2;
+struct bank
 {
-public:
+	char code[20];
+	char name[20];
+	char last[20];
+	char birt[20];
+	char age[10];
+	char t,p;
+	int money,sum;
+	
+}a,b,c;
+using namespace std;
 
-	LoginManager()
-	{
-		accessGranted = 0;
+void Open_book_bank(void);
+void Deposit_money(void);
+void Transaction_history(void);
+void Withdraw_money(void);
+void Show_datavoid(void);
+
+void Open_book_bank(void){
+	char type,ch;
+	
+	do{
+	system("cls");
+	cout << "--- Open book bank ---"<<endl;
+	fp = fopen("bank.dat","ab");
+	
+	cout << "ID Card : ";
+	cin >> a.code; fflush(stdin);
+	cout << "Name : ";
+	fflush(stdin);
+	cin >> a.name; fflush(stdin);
+	cout << "Last name : ";
+	cin >> a.last; fflush(stdin);
+	cout << "Birthday 00/00/0000 : ";
+	cin >> a.birt; fflush(stdin);
+	cout << "Age : ";
+	cin >> a.age; fflush(stdin);
+	cout << endl;
+	a.money=0;
+	cout << "-- open account type --"<<endl;
+	cout << "1.Fixed account"<<endl;
+	cout << "2.Current account"<<endl;
+	cout << "Enter type : ";
+	type = getche(); cout << endl;
+	
+	switch(type){
+		case '1': a.t ='F';break; //Fixed account
+		case '2': a.t ='C';break; //Current account
 	}
-	void login()
-	{
-		char PassAttempt[6];
-		cout << "\n\n\n\t  BANK";
-		cout << "\n\n\t  SYSTEM";
-		cout << "\n\n\n\nUser ";
-		cout << "\n\n\n\nUser ID :";
-		cin >> UserAttempt;
+	fwrite(&a,sizeof(a),1,fp);
+	cout <<endl<< "Press y/n to Add Data : ";
+		ch = getche();
+	}while(toupper(ch)=='Y');
+	fclose(fp);
+}
 
-		User = getFile("User.dat");
-		if (UserAttempt == User)
-		{
-			cout << "Password :";
-			cin >> PassAttempt;
-
-			Pass = getFile("Pass.dat");
-
-			if (PassAttempt == Pass)
-			{
-				cout << "Hey, that's right.";
-				cin.get();
+void Deposit_money(void){
+		char edit[13];
+	int n=1;
+	int num = sizeof(b);
+	system("cls");
+	if((fp = fopen("bank.dat","r+b"))==NULL){
+	cout<<"Cannot open file !\n";
+	}
+	else{
+		cout<<"----- Deposit money -----";
+		cout<<endl<<"Account number : ";
+		gets(edit);
+		int n=1;
+		cout<<endl<<"---ID Card-------Name-----Last--------Birt------Age-----------Money-----------Type----"<<endl;
+		for(;;){
+			if(fread(&a,sizeof(a),1,fp)!=1){
+				if(feof(fp)){
+					fclose(fp);
+					break;
+				}
 			}
-			else
-			{
-				cout << "Not even close." << endl;
-				login();
+			if(strcmp(a.code,edit)==0){
+				n++;
+				cout<<a.code<<"    "<<a.name<<"    "<<a.last<<"    "<<a.birt<<"    "<<a.age<<"           "<<a.money;
+				if(a.t=='F'){
+					cout<<"      Fixed account"<<endl;
+				}else if(a.t=='C'){
+					cout<<"      Current account"<<endl;
+				}
+				cout<<endl<<"----- Deposit money -----";
+				cout<<endl<<"Add Money = "; cin>>b.money;
+				a.money = (a.money + b.money);
+				a.sum = b.money;
+				a.p='+';
+				
+				if((fp2 = fopen("history.dat","ab"))==NULL){
+					cout << "Nofile bank.dat installing file..."<<endl<<endl;
+					ofstream myFile("history.dat",ios::out);
+					
+				}else{
+					fwrite(&a,sizeof(a),1,fp2);
+				}
+		
+				fseek(fp,-(num),SEEK_CUR);
+				fwrite(&a,sizeof(a),1,fp);
+				cout<<"----- Success !!! -----";
+				
+	    
+	
+				break;
+				
+			}	
+		}
+		if (n==1){
+				cout<<endl<<"                                       NO Data                                        "<<endl;
+				cout<<endl<<"--------------------------------------------------------------------------------------"<<endl;
 			}
-		}
-		else
-		{
-			cout << "Nice try, bud." << endl;
-			login();
-		}
 	}
-	string getFile(const char* p_fileName)
-	{
-		string line;
-		fstream file;
-
-		int eChar;
-
-		file.open(p_fileName, ios::in);
-
-		while (1)
-		{
-			file >> eChar;
-			if (eChar == 0)
-			{
-				file.close();
-				return line;
+	fclose(fp);
+	fclose(fp2);
+	getch();
+}
+void Withdraw_money(void){
+	char edit[13];
+	int n=1;
+	int num = sizeof(b);
+	system("cls");
+	if((fp = fopen("bank.dat","r+b"))==NULL){
+		cout<<"Cannot open file !";
+	}
+	else{
+		cout<<"----- Withdraw money -----";
+		cout<<endl<<"Account number : ";
+		gets(edit);
+		int n=1;
+		cout<<endl<<"---ID Card-------Name-----Last--------Birt------Age-----------Money-----------Type----"<<endl;
+		for(;;){
+			if(fread(&a,sizeof(a),1,fp)!=1){
+				if(feof(fp)){
+					fclose(fp);
+					break;
+				}
 			}
-			line += (char)decrypt(eChar);
+			if(strcmp(a.code,edit)==0){
+				n++;
+				cout<<a.code<<"    "<<a.name<<"    "<<a.last<<"    "<<a.birt<<"    "<<a.age<<"           "<<a.money;
+				if(a.t=='F'){
+					cout<<"      Fixed account"<<endl;
+				}else if(a.t=='C'){
+					cout<<"      Current account"<<endl;
+				}
+				cout<<"----- Withdraw money -----";
+				cout<<endl<<"Withdraw Money = "; cin>>b.money;
+				
+				if(b.money > a.money){
+					cout<<"--- The transaction cannot be made due to insufficient funds. ---";
+				}
+				else{
+					
+					
+					a.money = (a.money - b.money);
+					a.sum = b.money;
+					a.p='-';
+					if((fp2 = fopen("history.dat","ab"))==NULL){
+						cout << "Nofile bank.dat installing file..."<<endl<<endl;
+						ofstream myFile("history.dat",ios::out);
+					}else{
+						fwrite(&a,sizeof(a),1,fp2);
+					}
+				
+					fseek(fp,-(num),SEEK_CUR);
+					fwrite(&a,sizeof(a),1,fp);
+					cout<<"----- Success !!! -----";
+				}
+				break;
+			}	
 		}
+		if (n==1){
+				cout<<endl<<"                                       NO Data                                        "<<endl;
+				cout<<endl<<"--------------------------------------------------------------------------------------"<<endl;
+			}
 	}
-	void saveFile(string p_line, const char* p_fileName)
-	{
-		fstream file;
-		file.open(p_fileName, ios::out);
+	fclose(fp);
+	fclose(fp2);
+	getch();
+}
 
-		for (int i = 0; i < p_line.length(); i++)
-		{
-			file << encrypt(p_line[i]);
-			file << "\n";
+void Transaction_history(void){
+	int n=1;
+	system("cls");
+	if((fp = fopen("history.dat","rb"))==NULL){
+		printf("Cannot open file !\n");
+	}
+	else{
+		printf("----- History Data -----\n");
+		cout<<endl<<"---ID Card--------Money----------Toltal-------Type----"<<endl;
+		for(;;){
+			if(fread(&a,sizeof(a),1,fp)!=1){
+				if(feof(fp)){
+					fclose(fp);
+					break;
+				}
+			}	
+			n++;
+			//cout<<a.code<<"    "<<a.name<<"    "<<a.last<<"    "<<a.birt<<"    "<<a.age<<"           "<<a.money;
+			//printf("%s%9s%10s%15s%9s%16d",a.code,a.name,a.last,a.birt,a.age,a.money);
+			cout<<a.code<<"     "<<a.p<<a.sum;
+			printf("%16d",a.money);
+				if(a.t=='F'){
+					cout<<"      Fixed account"<<endl;
+				}else if(a.t=='C'){
+					cout<<"      Current account"<<endl;
+				}
+			
 		}
-
-		file << "0";
-		file.close();
+		if (n==1){    
+				cout<<endl<<"                                       NO Data                                        "<<endl;
+				cout<<endl<<"--------------------------------------------------------------------------------------"<<endl;
+			}
 	}
-
-	int encrypt(int p_letter)
-	{
-		return p_letter + 3;
+	fclose(fp);
+	getch();
+}
+void Show_data(void){
+	int n=1;
+	system("cls");
+	if((fp = fopen("bank.dat","rb"))==NULL){
+		printf("Cannot open file !\n");
 	}
-	int decrypt(int p_letter)
-	{
-		return p_letter - 3;
+	else{
+		printf("----- Show Data -----\n");
+		cout<<endl<<"---ID Card--------Name-----Last---------Birt---------Age-----------Money-----------Type----"<<endl;
+		for(;;){
+			if(fread(&a,sizeof(a),1,fp)!=1){
+				if(feof(fp)){
+					fclose(fp);
+					break;
+				}
+			}	
+			n++;
+			//cout<<a.code<<"    "<<a.name<<"    "<<a.last<<"    "<<a.birt<<"    "<<a.age<<"           "<<a.money;
+			printf("%s%9s%10s%15s%9s%16d",a.code,a.name,a.last,a.birt,a.age,a.money);
+				if(a.t=='F'){
+					cout<<"      Fixed account"<<endl;
+				}else if(a.t=='C'){
+					cout<<"      Current account"<<endl;
+				}
+			
+		}
+		if (n==1){    
+				cout<<endl<<"                                       NO Data                                        "<<endl;
+				cout<<endl<<"--------------------------------------------------------------------------------------"<<endl;
+			}
 	}
-
-	private:
-		string Pass;
-		string User;
-		string UserAttempt;
-		string PassAttempt;
-		bool accessGranted;
-	};
-
-
-int main() {
+	fclose(fp);
+	getch();
+}	
+int main(){
 	char ch;
-	int num;
-	login();
-	do
-	{
+	do{
+	ifstream f;
+	f.open("bank.dat");
+	if(f.is_open()){
 		system("cls");
-		cout << "\n\n\n\tMAIN MENU" << endl;
-		cout << "\n\n\t01. Open a new account" << endl;
-		cout << "\n\n\t02. Deposit money" << endl;
-		cout << "\n\n\t03. Withdraw money" << endl;
-		cout << "\n\n\t04. Show another" << endl;
-		cout << "\n\n\t05. EXIT << endl";
-		cout << "\n\n\tSelect Your Option (1-5) ";
-		cin >> ch;
-		system("cls");
-		switch (ch)
-		{
-		case '1':
-			ONA();
-			break;
-		case '2':
-			DM();
-			break;
-		case '3':
-			WM();
-			break;
-		case '4':
-			SA();
-			break;
-		case '5':
-			cout << "\n\n\tThanks for using bank ";
-			break;
-		default:cout << "\a";
-		}
-		cin.ignore();
-		cin.get();
-	} while (ch != '5');
-	return 0;
-}
-
-//Open_a_new_account
-void ONA() {
-
-}
-
-//Deposit_money
-void DM() {
-
-}
-
-//Withdraw_money
-void WM() {
-
-}
-
-//Show_another
-void SA() {
-
+		cout << "---Select number---"<<endl;
+		cout << "1.Open book bank"<<endl;
+		cout << "2.Deposit money"<<endl;
+		cout << "3.Withdraw money"<<endl;
+		cout << "4.Transaction history"<<endl;
+		cout << "5.Show data"<<endl;
+		cout<<"Press E to Exit !"<<endl;
+		cout << "-------------------"<<endl;
+		cout << "Enter number : ";
+		ch = getche(); cout <<endl<<endl;
+		
+		switch(ch){
+			case '1': Open_book_bank();break;
+			case '2': Deposit_money();break;
+			case '3': Withdraw_money();break;
+			case '4': Transaction_history();break;
+			case '5': Show_data();break;
+	}
+	}else{
+		cout << "Nofile bank.dat installing file..."<<endl<<endl;
+		ofstream myFile("bank.dat",ios::out);
+		cout << "Please run the program again."<<endl;
+	}
+	
+	}while(toupper(ch)!='E');
+	
+	return 0 ;
 }
